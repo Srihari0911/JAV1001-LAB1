af9178a665ed57073249d4bec08f3d7f59885efa
Srihari0911 <122223722+Srihari0911@users.noreply.github.com>
2023-02-02 21:02:01 -0500

    Created Convert.kt

---
+++ Convert.kt
@@ -0,0 +1,7 @@
+fun main() {
+
+    print("Enter a value: ")
+    val number = readLine()!!.toFloat()
+
+    print("Enter the unit of measurement you want to convert: ")
+    val measurementUnit = readLine()
