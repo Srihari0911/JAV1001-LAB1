a77af854a4bb367f88e78ba555663480bd4f1c0c
Srihari0911 <122223722+Srihari0911@users.noreply.github.com>
2023-02-02 21:02:39 -0500

    Update lab1

--- Convert.kt
+++ Convert.kt
@@ -5,3 +5,10 @@ fun main() {
 
     print("Enter the unit of measurement you want to convert: ")
     val measurementUnit = readLine()
+    
+    var result = ""
+    when (measurementUnit) {
+        "km" -> result = "$number$measurementUnit = ${0.62*number}mi"
+        "mi" -> result = "$number$measurementUnit = ${1.61*number}km"
+        "cm" -> result = "$number$measurementUnit = ${0.39*number}in"
+        "in" -> result = "$number$measurementUnit = ${2.54*number}cm"
