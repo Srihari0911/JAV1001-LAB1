1606478e2f9e5d2e62e66a641dc8073ea718139c
Srihari0911 <122223722+Srihari0911@users.noreply.github.com>
2023-02-02 21:05:54 -0500

    Worked on mass conversion

--- Convert.kt
+++ Convert.kt
@@ -12,3 +12,7 @@ fun main() {
         "mi" -> result = "$number$measurementUnit = ${1.61*number}km"
         "cm" -> result = "$number$measurementUnit = ${0.39*number}in"
         "in" -> result = "$number$measurementUnit = ${2.54*number}cm"
+        "kg" -> result = "$number$measurementUnit = ${2.2*number}lb"
+        "lb" -> result = "$number$measurementUnit = ${0.45*number}kg"
+        "g"  -> result = "$number$measurementUnit = ${0.04*number}oz"
+        "oz" -> result = "$number$measurementUnit = ${28.35*number}g"
