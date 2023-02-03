b446d9ec0df0fd1ea014ee1695433c9a9ced1e1e
Srihari0911 <122223722+Srihari0911@users.noreply.github.com>
2023-02-02 21:07:40 -0500

    Made some changes

--- Convert.kt
+++ Convert.kt
@@ -1,10 +1,10 @@
 fun main() {
 
-    print("Enter a value: ")
-    val number = readLine()!!.toFloat()
-
     print("Enter the unit of measurement you want to convert: ")
     val measurementUnit = readLine()
+    
+     print("Enter a value: ")
+    val number = readLine()!!.toFloat()
     
     var result = ""
     when (measurementUnit) {
